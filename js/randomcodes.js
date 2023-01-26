0cbc5b4ceaebb647bf3d9ed136d4eec4e4234e06
[WilliamWangWalnutTheAdvantureCat] <[william.wang1986@icloud.com]>
2023-01-26 13:50:46 -0600

    Added random codes, adjusted inline display, disabled button, all set for day1 homework.

---
+++ js/randomcodes.js
@@ -0,0 +1,26 @@
+/* Randome Codes */
+
+//Function to generate combination of characters
+function generateCode() {
+    //Create variables to store genearted codes and the type of characters we want to show as codes
+    var code = " "; //initialize to null value
+    var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$";
+
+    //Generate character mutiple times using a loop
+    for (var i=1; i<=8; i++) {
+        var char = Math.random()* str.length; //random select a character from the variable and then store in a new variable
+        code +=str.charAt(char) //accumulate the generated character into a string of 8 characters
+    }
+    return code; //return the final accumulated string when loop ends
+}
+
+//Get HTML element to display
+document.getElementById("codes").innerHTML = generateCode();
+
+//Disable Button
+function disableButton() {
+    document.getElementById("submit").disabled = true;
+}
+
+//Activate function
+disableButton();
