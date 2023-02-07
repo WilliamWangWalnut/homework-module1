228ec03eba6629f44647fe53a6e35f1246b8e7fd
[WilliamWangWalnutTheAdvantureCat] <[william.wang1986@icloud.com]>
2023-02-07 14:37:37 -0600

    Site Indicator completed, I was using :nth-child(index()) method, instead of the eq() method

--- js/activity.js
+++ js/activity.js
@@ -1,38 +1,16 @@
 // Toggle Table Events
-$(document).ready(function() {
-    $("*#events").click(function() {
-        $(this).toggleClass("active");
-    })
-})
+$(document).ready(function () {
+  $("*#events").click(function () {
+    $(this).toggleClass("active");
+  });
+});
 
 //User interaction with table cells
-// $(document).read(function() {
-//     $("td").click(function() {
-//         var content = $(this).text();
-
-//         if (content != "Not Available") {
-//             $(this).toggleClass("tdhighlight");
-
-//             if ($(this).hasClass("tdhighlight")) {
-//                 $('#displaySelected').css("visibility","visible");
-//                 $('#displaySelected').css("margin-top","2em");
-//                 $('#result').append("<p>"+content+"</p>");
-
-//             } else {
-//                 $('#result p:contains('+content+')').remove();
-
-//                 if ($('#result').has('p').length==false) {
-//                     $('#displaySelected').css("visibility","hidden");
-//                     $('#displaySelected').css("margin-top","0");
-//                 }
-//             }
-//         }
-//     });
-// });
-
 $(document).ready(function () {
-  $("td").click(function () {
+  $("*#events").click(function () {
     var content = $(this).text();
+    var index = $(this).index(); //Show Cliff Sites based on User interaction
+    var site = $(".site:nth-child(" + (index + 1) + ")").text();
 
     if (content != "Not Available") {
       $(this).toggleClass("tdhighlight");
@@ -40,9 +18,9 @@ $(document).ready(function () {
       if ($(this).hasClass("tdhighlight")) {
         $("#displaySelected").css("visibility", "visible");
         $("#displaySelected").css("margin-top", "2em");
-        $("#result").append("<p>" + content + "</p>");
+        $("#result").append("<p>" + content + " at " + site + "</p>");
       } else {
-        $("#result p:contains(" + content + ")").remove();
+        $("#result p:contains(" + content + " at " + site + ")").remove();
 
         if ($("#result").has("p").length == false) {
           $("#displaySelected").css("visibility", "hidden");
