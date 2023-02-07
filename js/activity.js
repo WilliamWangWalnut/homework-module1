c63bf8859a64794cf36fdbcdcbd337939ea09a22
[WilliamWangWalnutTheAdvantureCat] <[william.wang1986@icloud.com]>
2023-02-07 01:34:52 -0600

    display box ready, need to add cliff sites indicator

--- js/activity.js
+++ js/activity.js
@@ -1,5 +1,54 @@
+// Toggle Table Events
 $(document).ready(function() {
     $("*#events").click(function() {
         $(this).toggleClass("active");
     })
 })
+
+//User interaction with table cells
+// $(document).read(function() {
+//     $("td").click(function() {
+//         var content = $(this).text();
+
+//         if (content != "Not Available") {
+//             $(this).toggleClass("tdhighlight");
+
+//             if ($(this).hasClass("tdhighlight")) {
+//                 $('#displaySelected').css("visibility","visible");
+//                 $('#displaySelected').css("margin-top","2em");
+//                 $('#result').append("<p>"+content+"</p>");
+
+//             } else {
+//                 $('#result p:contains('+content+')').remove();
+
+//                 if ($('#result').has('p').length==false) {
+//                     $('#displaySelected').css("visibility","hidden");
+//                     $('#displaySelected').css("margin-top","0");
+//                 }
+//             }
+//         }
+//     });
+// });
+
+$(document).ready(function () {
+  $("td").click(function () {
+    var content = $(this).text();
+
+    if (content != "Not Available") {
+      $(this).toggleClass("tdhighlight");
+
+      if ($(this).hasClass("tdhighlight")) {
+        $("#displaySelected").css("visibility", "visible");
+        $("#displaySelected").css("margin-top", "2em");
+        $("#result").append("<p>" + content + "</p>");
+      } else {
+        $("#result p:contains(" + content + ")").remove();
+
+        if ($("#result").has("p").length == false) {
+          $("#displaySelected").css("visibility", "hidden");
+          $("#displaySelected").css("margin-top", "0");
+        }
+      }
+    }
+  });
+});
